eda79af2ce8244f103bca92160a43b4e8a7f28db
Antoine Mirloup <amirloup@k0r1p8.42mulhouse.fr>
2024-02-02 16:00:02 +0100

    save 02/02

--- map_check.c
+++ map_check.c
@@ -6,7 +6,7 @@
 /*   By: amirloup <amirloup@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 10:50:28 by amirloup          #+#    #+#             */
-/*   Updated: 2024/02/02 15:09:57 by amirloup         ###   ########.fr       */
+/*   Updated: 2024/02/02 15:59:23 by amirloup         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,47 @@ char	**get_map(char **argv)
 	return (map);
 }
 
+int	check_walls(char **argv)
+{
+	char	**map;
+	int		x;
+	int		y;
+
+	x = 0;
+	y = 0;
+	map = get_map(argv);
+	while (map[y] != NULL)
+	{
+		if (y == 0 || y == count_y(argv - 1))
+		{
+			while (map[y][x] != '\n' && map[y][x] != '\0')
+			{
+				if (map[y][x] != '1')
+				{
+					free_tab(map);
+					exit((perror("Wall got a hole!"), EXIT_FAILURE));
+				}
+				x++;
+			}
+		}
+		else
+		{
+			while (map[y][x] != '\n' && map[y][x] != '\0')
+			{
+				if (map[y][0] != '1' || map[y][ft_strlen(map[y])] != '1')
+				{
+					free_tab(map);
+					exit((perror("Wall got a hole!"), EXIT_FAILURE));
+				}
+				x++;
+			}
+		}
+		y++;
+	}
+	free_tab(map);
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	int		i;
@@ -67,12 +108,14 @@ int	main(int argc, char **argv)
 	map = get_map(argv);
 	if (argc > 1)
 	{
-		while (map[i])
+		if (check_walls(argv) == 1)
 		{
-			printf("%s", map[i]);
-			i++;
+			while (map[i])
+			{
+				printf("%s", map[i]);
+				i++;
+			}
+			free_tab(map);
 		}
-		free_tab(map);
 	}
-	return (EXIT_SUCCESS);
 }
